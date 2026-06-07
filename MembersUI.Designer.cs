49f94f720669da1bd52a6e1944ac976557a48606
Mysrerio-us <bahardeenayas8@gmail.com>
2026-06-07 12:43:42 +0530

    UI initial responsiveness

--- MembersUI.Designer.cs
+++ MembersUI.Designer.cs
@@ -28,52 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MembersUI));
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.roundControl1 = new FOT_BFMS.RoundControl();
-            this.roundControl3 = new FOT_BFMS.RoundControl();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.roundControl3 = new FOT_BFMS.RoundControl();
+            this.roundControl1 = new FOT_BFMS.RoundControl();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // roundControl1
-            // 
-            this.roundControl1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.roundControl1.AutoScroll = true;
-            this.roundControl1.BackColor = System.Drawing.Color.Navy;
-            this.roundControl1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.roundControl1.BorderColor = System.Drawing.SystemColors.Control;
-            this.roundControl1.BorderWidth = 1F;
-            this.roundControl1.Location = new System.Drawing.Point(941, 6);
-            this.roundControl1.Margin = new System.Windows.Forms.Padding(2);
-            this.roundControl1.Name = "roundControl1";
-            this.roundControl1.Radius = 10;
-            this.roundControl1.Size = new System.Drawing.Size(69, 77);
-            this.roundControl1.TabIndex = 3;
-            // 
-            // roundControl3
-            // 
-            this.roundControl3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.roundControl3.AutoScroll = true;
-            this.roundControl3.AutoSize = true;
-            this.roundControl3.BackColor = System.Drawing.Color.Navy;
-            this.roundControl3.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.roundControl3.BorderColor = System.Drawing.SystemColors.Control;
-            this.roundControl3.BorderWidth = 1F;
-            this.roundControl3.ForeColor = System.Drawing.Color.Transparent;
-            this.roundControl3.Location = new System.Drawing.Point(1032, 4);
-            this.roundControl3.Margin = new System.Windows.Forms.Padding(2);
-            this.roundControl3.Name = "roundControl3";
-            this.roundControl3.Radius = 10;
-            this.roundControl3.Size = new System.Drawing.Size(298, 79);
-            this.roundControl3.TabIndex = 2;
             // 
             // panel1
             // 
@@ -86,57 +55,22 @@
             this.panel1.Controls.Add(this.roundControl1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1424, 94);
+            this.panel1.Size = new System.Drawing.Size(1780, 106);
             this.panel1.TabIndex = 4;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Image = global::FOT_BFMS.Properties.Resources.notification;
-            this.pictureBox1.Location = new System.Drawing.Point(958, 23);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 45);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(59, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 38);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "BFMS";
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
             this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 88);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 104);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1424, 774);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1780, 968);
             this.flowLayoutPanel1.TabIndex = 5;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = global::FOT_BFMS.Properties.Resources.logout__1_;
-            this.pictureBox2.Location = new System.Drawing.Point(1356, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(36, 62);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 8;
-            this.pictureBox2.TabStop = false;
             // 
             // tableLayoutPanel1
             // 
@@ -144,31 +78,105 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 100);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1000, 125);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::FOT_BFMS.Properties.Resources.logout__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(1720, 13);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(46, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::FOT_BFMS.Properties.Resources.notification;
+            this.pictureBox1.Location = new System.Drawing.Point(1272, 30);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(74, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BFMS";
+            // 
+            // roundControl3
+            // 
+            this.roundControl3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundControl3.AutoScroll = true;
+            this.roundControl3.AutoSize = true;
+            this.roundControl3.BackColor = System.Drawing.Color.Navy;
+            this.roundControl3.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.roundControl3.BorderColor = System.Drawing.SystemColors.Control;
+            this.roundControl3.BorderWidth = 1F;
+            this.roundControl3.ForeColor = System.Drawing.Color.Transparent;
+            this.roundControl3.Location = new System.Drawing.Point(1331, 11);
+            this.roundControl3.Margin = new System.Windows.Forms.Padding(2);
+            this.roundControl3.Name = "roundControl3";
+            this.roundControl3.Radius = 10;
+            this.roundControl3.Size = new System.Drawing.Size(372, 57);
+            this.roundControl3.TabIndex = 2;
+            // 
+            // roundControl1
+            // 
+            this.roundControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundControl1.AutoScroll = true;
+            this.roundControl1.BackColor = System.Drawing.Color.Navy;
+            this.roundControl1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.roundControl1.BorderColor = System.Drawing.SystemColors.Control;
+            this.roundControl1.BorderWidth = 1F;
+            this.roundControl1.Location = new System.Drawing.Point(1255, 11);
+            this.roundControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.roundControl1.Name = "roundControl1";
+            this.roundControl1.Radius = 10;
+            this.roundControl1.Size = new System.Drawing.Size(63, 63);
+            this.roundControl1.TabIndex = 3;
             // 
             // MembersUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1424, 861);
+            this.ClientSize = new System.Drawing.Size(1780, 1055);
             this.Controls.Add(this.panel1);
-            this.MaximizeBox = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "MembersUI";
             this.Text = "MembersUI";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
